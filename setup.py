6dc3a0c0c8cc09d7a5397f040eb2a8fea3636fa4
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-01-30 02:58:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="TgBee",
-    version="1.0.9",
+    version="1.1.0",
     author="Ahmed Negm",
     author_email="a7mednegm.x@gmail.com",
     description="An asynchronous Python Telegram Bot API wrapper",
